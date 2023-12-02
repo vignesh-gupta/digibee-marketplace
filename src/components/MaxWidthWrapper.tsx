beade93d300884680df39749db1d5f5bff3926dd
Vignesh Gupta <vighneshgupta32@gmail.com>
2023-12-02 21:04:45 +0530

    initial setup

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,24 @@
+import { cn } from "@/lib/utils";
+import { ReactNode } from "react";
+import { ClassNameValue } from "tailwind-merge";
+
+const MaxWidthWrapper = ({
+  className,
+  children,
+}: {
+  className?: ClassNameValue;
+  children: ReactNode;
+}) => {
+  return (
+    <div
+      className={cn(
+        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default MaxWidthWrapper;
