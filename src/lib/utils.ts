3e2af0cda676950e05accf7effadd31d907499cd
Vignesh Gupta <vighneshgupta32@gmail.com>
2023-12-10 02:11:04 +0530

    Add Metadata + Favicon

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -22,4 +23,44 @@ export function formatPrice(
     notation,
     maximumFractionDigits: 2,
   }).format(numericPrice);
+}
+
+export function constructMetadata({
+  title = 'DigiBee - the marketplace for digital assets',
+  description = 'DigiBee is an open-source marketplace for high-quality digital goods.',
+  image = '/thumbnail.png',
+  noIndex = false,
+}: {
+  title?: string
+  description?: string
+  image?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description,
+      images: [image],
+      creator: '@joshtriedcoding',
+    },
+    metadataBase: new URL('https://digiBee.up.railway.app'),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  }
 }
