b4bdafaec71c7b223174a2a4f51832ad59f9ae78
Vignesh Gupta <vighneshgupta32@gmail.com>
2023-12-12 02:51:52 +0530

    Use Aws S3 for Media files

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -5,8 +5,24 @@ import { mongooseAdapter } from "@payloadcms/db-mongodb";
 import { webpackBundler } from "@payloadcms/bundler-webpack";
 import path from "path";
 import { Media, Orders, ProductFiles, Products, Users } from "./collections";
+import { cloudStorage } from "@payloadcms/plugin-cloud-storage";
+import { s3Adapter } from "@payloadcms/plugin-cloud-storage/s3";
 
 dotenv.config({ path: path.resolve(__dirname, "../.env") });
+
+const saveToS3Folder = (folder: string) => {
+  return s3Adapter({
+    config: {
+      credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY!,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+      },
+      region: process.env.AWS_BUCKET_REGION!,
+      // ... Other S3 configuration
+    },
+    bucket: process.env.AWS_BUCKET_NAME!,
+  });
+};
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || "",
@@ -33,4 +49,16 @@ export default buildConfig({
   typescript: {
     outputFile: path.resolve(__dirname, "payload-types.ts"),
   },
+  plugins: [
+    cloudStorage({
+      collections: {
+        media: {
+          adapter: saveToS3Folder("media"),
+        },
+        product_files: {
+          adapter: saveToS3Folder("product_files"),
+        },
+      },
+    }),
+  ],
 });
