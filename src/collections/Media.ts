dcd9ad8a79aa9fc2d01a0289328fb315bea3532c
Vignesh Gupta <vighneshgupta32@gmail.com>
2023-12-12 02:51:52 +0530

    Use Aws S3 for Media files

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -1,5 +1,9 @@
 import { User } from "../payload-types";
-import { Access, CollectionConfig } from "payload/types";
+import {
+  Access,
+  CollectionConfig,
+  CollectionBeforeOperationHook,
+} from "payload/types";
 
 const isAdminOrHasAccessToImage =
   (): Access =>
@@ -36,6 +40,18 @@ const Media: CollectionConfig = {
     update: isAdminOrHasAccessToImage(),
   },
   hooks: {
+    beforeOperation: [
+      async ({ args, operation }) => {
+        const files = args.req?.files;
+        if (files && files.file && files.file.name && operation === "create") {
+          const parts = files.file.name.split(".");
+          files.file.name = 
+          `${(Math.random() + 1).toString(36).substring(2)}-
+          ${Math.random().toString(36).substring(2, 15)}.
+          ${parts[parts.length - 1]}`;
+        }
+      },
+    ],
     beforeChange: [
       ({ req, data }) => {
         return { ...data, user: req.user.id };
