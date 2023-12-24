f65538f4b661b1c21c009377b29f7fc680f75d89
Vignesh Gupta <vighneshgupta32@gmail.com>
2023-12-25 00:03:58 +0530

    rm lock files

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -40,23 +40,23 @@ const Cart = () => {
 
   return (
     <Sheet>
-      <SheetTrigger className="group -m-2 flex items-center p-2">
+      <SheetTrigger className="flex items-center p-2 -m-2 group">
         <ShoppingCart
           aria-hidden="true"
-          className="h-6 w-6 text-gray-400 flex-shrink-0 group-hover:text-gray-500"
+          className="flex-shrink-0 w-6 h-6 text-muted-foreground/70 group-hover:text-muted-foreground"
         />
-        <span className="text-sm font-medium text-gray-700 group-hover:text-gray-800 ml-2">
+        <span className="ml-2 text-sm font-medium text-muted-foreground/70 group-hover:text-muted-foreground">
           {itemCount}
         </span>
       </SheetTrigger>
 
-      <SheetContent className="flex w-full flex-col pr-0 sm:max-w-lg">
+      <SheetContent className="flex flex-col w-full pr-0 sm:max-w-lg">
         <SheetHeader className="space-y-2.5 pr-6">
           <SheetTitle>Cart {itemCount}</SheetTitle>
         </SheetHeader>
         {itemCount > 0 ? (
           <>
-            <div className="flex w-full flex-col pr-6">
+            <div className="flex flex-col w-full pr-6">
               {/* Cart Logic */}
               <ScrollArea>
                 {items.map(({ product }) => (
@@ -64,7 +64,7 @@ const Cart = () => {
                 ))}
               </ScrollArea>
             </div>
-            <div className="space-y-4 pr-6">
+            <div className="pr-6 space-y-4">
               <Separator />
               <div className="space-y-1.5 text-sm">
                 <div className="flex">
@@ -93,8 +93,8 @@ const Cart = () => {
             </div>
           </>
         ) : (
-          <div className="flex justify-center items-center h-full flex-col space-y-1">
-            <div className="relative w-60 h-60 text-muted-foreground mb-4">
+          <div className="flex flex-col items-center justify-center h-full space-y-1">
+            <div className="relative mb-4 w-60 h-60 text-muted-foreground">
               <Image
                 src="/hippo-empty-cart.png"
                 fill
