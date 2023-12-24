c84b0a1e87654072b6a3993c7fd9c25111975523
Vignesh Gupta <vighneshgupta32@gmail.com>
2023-12-24 17:26:46 +0530

    refactor auth layout

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -72,98 +72,90 @@ const SignInPage = () => {
 
   return (
     <>
-      <div className="container relative flex flex-col items-center justify-center pt-20 lg:px-0">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className="flex flex-col items-center space-y-2 text-center">
-            <div className="w-24 h-24 relative">
-              <Image src="/logo.png" fill alt="Digibee Logo" />
-            </div>
-            <h1 className="text-2xl font-semibold tracking-tight">
-              Sign in to your {isSeller ? "seller" : ""} account
-            </h1>
+      <div className="flex flex-col items-center space-y-2 text-center">
+        <div className="relative w-24 h-24">
+          <Image src="/logo.png" fill alt="Digibee Logo" />
+        </div>
+        <h1 className="text-2xl font-semibold tracking-tight">
+          Sign in to your {isSeller ? "seller" : ""} account
+        </h1>
 
-            <Link
-              className={buttonVariants({
-                variant: "link",
-                className: "gap-1.5",
-              })}
-              href="/sign-up"
-            >
-              Don&apos;t have an account?
-              <ArrowRight className="w-4 h-4" />
-            </Link>
+        <Link
+          className={buttonVariants({
+            variant: "link",
+            className: "gap-1.5",
+          })}
+          href="/sign-up"
+        >
+          Don&apos;t have an account?
+          <ArrowRight className="w-4 h-4" />
+        </Link>
+      </div>
+
+      <div className="grid gap-6">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="grid gap-2">
+            <div className="grid gap-1 py-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                {...register("email")}
+                className={cn({
+                  "focus-visible:ring-red-500": errors.email,
+                })}
+                placeholder="you@example.com"
+              />
+              {errors?.email && (
+                <p className="text-sm text-red-500">{errors?.email?.message}</p>
+              )}
+            </div>
+
+            <div className="grid gap-1 py-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                {...register("password")}
+                type="password"
+                className={cn({
+                  "focus-visible:ring-red-500": errors.password,
+                })}
+                placeholder="Password"
+              />
+              {errors?.password && (
+                <p className="text-sm text-red-500">
+                  {errors?.password?.message}
+                </p>
+              )}
+            </div>
+
+            <Button>Sign in</Button>
           </div>
+        </form>
 
-          <div className="grid gap-6">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="grid gap-2">
-                <div className="grid gap-1 py-2">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    {...register("email")}
-                    className={cn({
-                      "focus-visible:ring-red-500": errors.email,
-                    })}
-                    placeholder="you@example.com"
-                  />
-                  {errors?.email && (
-                    <p className="text-sm text-red-500">
-                      {errors?.email?.message}
-                    </p>
-                  )}
-                </div>
-
-                <div className="grid gap-1 py-2">
-                  <Label htmlFor="password">Password</Label>
-                  <Input
-                    {...register("password")}
-                    type="password"
-                    className={cn({
-                      "focus-visible:ring-red-500": errors.password,
-                    })}
-                    placeholder="Password"
-                  />
-                  {errors?.password && (
-                    <p className="text-sm text-red-500">
-                      {errors?.password?.message}
-                    </p>
-                  )}
-                </div>
-
-                <Button>Sign in</Button>
-              </div>
-            </form>
-
-            <div className="relative">
-              <div className="absolute inset-0 flex items-center">
-                <span aria-hidden="true" className="w-full border-t" />
-              </div>
-              <div className="relative flex justify-center text-xs uppercase">
-                <span className="px-2 bg-background text-muted-foreground">
-                  or
-                </span>
-              </div>
-            </div>
-
-            {!isSeller ? (
-              <Button
-                variant="secondary"
-                disabled={isLoading}
-                onClick={() => continueAsRole(true)}
-              >
-                Continue as Seller
-              </Button>
-            ) : (
-              <Button
-                variant="secondary"
-                disabled={isLoading}
-                onClick={() => continueAsRole()}
-              >
-                Continue as Customer
-              </Button>
-            )}
+        <div className="relative">
+          <div className="absolute inset-0 flex items-center">
+            <span aria-hidden="true" className="w-full border-t" />
+          </div>
+          <div className="relative flex justify-center text-xs uppercase">
+            <span className="px-2 bg-background text-muted-foreground">or</span>
           </div>
         </div>
+
+        {!isSeller ? (
+          <Button
+            variant="secondary"
+            disabled={isLoading}
+            onClick={() => continueAsRole(true)}
+          >
+            Continue as Seller
+          </Button>
+        ) : (
+          <Button
+            variant="secondary"
+            disabled={isLoading}
+            onClick={() => continueAsRole()}
+          >
+            Continue as Customer
+          </Button>
+        )}
       </div>
     </>
   );
--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -60,69 +60,63 @@ const SignUpPage = () => {
 
   return (
     <>
-      <div className="container relative flex flex-col items-center justify-center pt-20 lg:px-0">
-        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
-          <div className="flex flex-col items-center space-y-2 text-center">
-            <div className="w-24 h-24 relative">
-              <Image src="/logo.png" fill alt="Digibee Logo" />
-            </div>
-            <h1 className="text-2xl font-semibold tracking-tight">
-              Create an account
-            </h1>
-
-            <Link
-              className={buttonVariants({
-                variant: "link",
-                className: "gap-1.5",
-              })}
-              href="/sign-in"
-            >
-              Already have an account?
-              <ArrowRight className="w-4 h-4" />
-            </Link>
-          </div>
-
-          <div className="grid gap-6">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <div className="grid gap-2">
-                <div className="grid gap-1 py-2">
-                  <Label htmlFor="email">Email</Label>
-                  <Input
-                    {...register("email")}
-                    className={cn({
-                      "focus-visible:ring-red-500": errors.email,
-                    })}
-                    placeholder="you@example.com"
-                  />
-                  {errors?.email && (
-                    <p className="text-sm text-red-500">
-                      {errors?.email?.message}
-                    </p>
-                  )}
-                </div>
-
-                <div className="grid gap-1 py-2">
-                  <Label htmlFor="password">Password</Label>
-                  <Input
-                    {...register("password")}
-                    type="password"
-                    className={cn({
-                      "focus-visible:ring-red-500": errors.password,
-                    })}
-                    placeholder="Password"
-                  />
-                  {errors?.password && (
-                    <p className="text-sm text-red-500">
-                      {errors?.password?.message}
-                    </p>
-                  )}
-                </div>
-
-                <Button>Sign up</Button>
-              </div>
-            </form>
-          </div>
+      <div className="flex flex-col items-center space-y-2 text-center">
+        <div className="relative w-24 h-24">
+          <Image src="/logo.png" fill alt="Digibee Logo" />
         </div>
+        <h1 className="text-2xl font-semibold tracking-tight">
+          Create an account
+        </h1>
+
+        <Link
+          className={buttonVariants({
+            variant: "link",
+            className: "gap-1.5",
+          })}
+          href="/sign-in"
+        >
+          Already have an account?
+          <ArrowRight className="w-4 h-4" />
+        </Link>
+      </div>
+
+      <div className="grid gap-6">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className="grid gap-2">
+            <div className="grid gap-1 py-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                {...register("email")}
+                className={cn({
+                  "focus-visible:ring-red-500": errors.email,
+                })}
+                placeholder="you@example.com"
+              />
+              {errors?.email && (
+                <p className="text-sm text-red-500">{errors?.email?.message}</p>
+              )}
+            </div>
+
+            <div className="grid gap-1 py-2">
+              <Label htmlFor="password">Password</Label>
+              <Input
+                {...register("password")}
+                type="password"
+                className={cn({
+                  "focus-visible:ring-red-500": errors.password,
+                })}
+                placeholder="Password"
+              />
+              {errors?.password && (
+                <p className="text-sm text-red-500">
+                  {errors?.password?.message}
+                </p>
+              )}
+            </div>
+
+            <Button>Sign up</Button>
+          </div>
+        </form>
       </div>
     </>
   );
