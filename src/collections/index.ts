8d5fed575ce617189b429a47e746350883da5808
Vignesh Gupta <vighneshgupta32@gmail.com>
2023-12-31 17:06:40 +0530

    Add Cart collection and update payload types

--- src/collections/index.ts
+++ src/collections/index.ts
@@ -3,3 +3,4 @@ export { default as Users } from "./Users";
 export { default as Products } from "./Products";
 export { default as ProductFiles } from "./ProductFile";
 export { default as Orders } from "./Orders";
+export { default as Cart } from "./Cart";
