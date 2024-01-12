5e40d823603dce1e5d260f2ab82461cbf451a291
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-01-12 11:26:13 +0530

    Update the mail sender to custom domain

--- src/get-payload.ts
+++ src/get-payload.ts
@@ -37,7 +37,7 @@ export const getPayloadClient = async ({
     cached.promise = payload.init({
       email: {
         transport: transporter,
-        fromAddress: "onboarding@resend.dev",
+        fromAddress: "hello-digibee@vigneshgupta.tech",
         fromName: "DigiBee",
       },
       secret: process.env.PAYLOAD_SECRET,
