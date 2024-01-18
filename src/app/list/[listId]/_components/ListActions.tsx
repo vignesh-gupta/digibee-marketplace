1793be95e899f908a724284c6c5af64f33d068d6
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-01-18 12:49:18 +0530

    Add deletion flow + toast to notify

--- src/app/list/[listId]/_components/ListActions.tsx
+++ src/app/list/[listId]/_components/ListActions.tsx
@@ -1,31 +1,26 @@
 import { TooltipProvider } from "@/components/ui/tooltip";
+import useListActions from "@/hooks/use-list-actions";
 import { List, User } from "@/payload-types";
 import { Copy, Edit, Trash } from "lucide-react";
 import TooltipButton from "./TooltipButton";
-import { trpc } from "@/trpc/client";
-import { useRouter } from "next/navigation";
 
 const ListActions = ({ user, list }: { user: User; list: List }) => {
   const isOwner =
     user.id === (typeof list.user === "string" ? list.user : list.user.id);
 
-  const router = useRouter();
+  const { copyList, deleteList } = useListActions();
 
-  const { mutate: copyList } = trpc.list.createList.useMutation({
-    onSuccess: ({ listId }) => {
-      console.log("success");
-      router.push(`/list/${listId}`);
-    },
-    onError: (error) => {
-      console.log(error);
-    },
-  });
-
-  const createCopyOfList = () => {
+  const onCopyHandler = () => {
     copyList({
       productIds: list.products?.map((product) =>
         typeof product === "string" ? product : product.id
       ),
+    });
+  };
+
+  const onDeleteHandler = () => {
+    deleteList({
+      id: list.id,
     });
   };
 
@@ -35,7 +30,7 @@ const ListActions = ({ user, list }: { user: User; list: List }) => {
         {!isOwner ? (
           <TooltipButton
             tooltipLabel="Create a copy of list"
-            onClick={createCopyOfList}
+            onClick={onCopyHandler}
           >
             <Copy className="h-4 w-4" />
           </TooltipButton>
@@ -45,7 +40,7 @@ const ListActions = ({ user, list }: { user: User; list: List }) => {
               <Edit className="h-4 w-4" />
             </TooltipButton>
 
-            <TooltipButton tooltipLabel="Delete list">
+            <TooltipButton onClick={onDeleteHandler} tooltipLabel="Delete list">
               <Trash className="h-4 w-4" />
             </TooltipButton>
           </>
