c47815cd30f2390478fbdebad17f03530cc3c000
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-01-22 09:20:19 +0530

    Update unauthenticated user routing

---
+++ src/lib/routes.ts
@@ -0,0 +1,10 @@
+export const AUTH_ROUTES = [
+  "/sign-in",
+  "/sign-up",
+  "/forgot-password",
+  "/reset-password",
+  "/verify",
+];
+
+export const PRIVATE_ROUTES = ["/cart", "/thank-you"];
+export const LIST_EDIT_PATTERN = /^\/list\/([^\/]+)\/edit$/;
