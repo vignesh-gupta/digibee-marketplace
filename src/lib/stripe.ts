36327bcd232bdbdf88166147ce5be2ed52b5e8b5
Vignesh Gupta <vighneshgupta32@gmail.com>
2024-05-22 11:00:43 +0530

    fix: :green_heart: update apiVersion of stripe

--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -1,7 +1,6 @@
 import Stripe from "stripe";
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY ?? "", {
-  apiVersion: "2023-10-16",
+  apiVersion: "2024-04-10",
   typescript: true,
-  
 });
